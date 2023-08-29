24f8df94ca83453507a3867cc407b69fbbf0fc56
stillaiko <ana.ribeiro5@estudante.ifms.edu.br>
2023-08-29 11:46:06 -0400

    eu sou linda

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Header from './componentes/header/Header'
 import Footer from './componentes/footer/Footer'
+import Main from './componentes/main/Main'
 
 
 function App() {
   return (
    <>
    <Header />
+   <Main />
    <Footer />
    </>
   )
--- src/componentes/footer/Footer.tsx
+++ src/componentes/footer/Footer.tsx
@@ -1,34 +1,40 @@
 export default Footer
-
+import twin1 from '../../assets/twin1.png'
+import twin2 from '../../assets/twin2.webp'
 import './Footer.css'
 
 function Footer(){
     return(
        <footer>
     
-    <nav>
-        <div>
+    <div className='box'>
 
+        <img className='twin1' src={twin1} alt="" />
+        
+        <nav>
+ 
          <div className='icons'>
          <div className='icon1'></div>
          <div className='icon2'></div>
+         <div className='icon3'></div>
          </div>
          
-        
-
          <ul className='ul'>
-         <li><a href="">DESINGS</a></li>
-         <li><a href="">CONTATO</a></li>
-         <li><a href="">SOBRE MIM</a></li>
-         <li><a href="">FEEDBACKS</a></li>
+         <li><a className='a' href="">DESINGS</a></li>
+         <li><a className='ponto' href="">.</a></li>
+         <li><a className='a' href="">CONTATO</a></li>
+         <li><a className='ponto' href="">.</a></li>
+         <li><a className='a' href="">SOBRE MIM</a></li>
+         <li><a className='ponto' href="">.</a></li>
+         <li><a className='a' href="">FEEDBACKS</a></li>
          </ul>
 
-         <div className='L'></div>
          <div className='mattel'>©2023</div>
-
-        </div>
-          
     </nav>
+
+    <img className='twin2' src={twin2} alt="" />
+
+    </div>
     
        </footer>
     )
