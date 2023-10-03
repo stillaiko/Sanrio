c32b6bff87fffbb94e2a2b065d2d88481f1fce1d
stillaiko <ana.ribeiro5@estudante.ifms.edu.br>
2023-10-03 11:43:42 -0400

    save

--- playground-1.mongodb.js
+++ playground-1.mongodb.js
@@ -1,48 +1,90 @@
-use("MyPet");
+use("Portifolio");
+db.dropDatabase()
 
-db.createCollection("dono");
-db.createCollection("animal");
+db.createCollection("trabalhos");
+db.createCollection("contratos");
 
+db.trabalhos.insertMany(
+    {
+        id:1,
+        titulo:"Formulário de cadastro",
+        descricao:"Formulário de cadastro de site em cores neutras", 
+        imagem:"/login_black.png"
+    },
+    {
+        id:2,
+        titulo:"Formulário de cadastro",
+        descricao:"Formulário de cadastro para rede social no tema azul", 
+        imagem:"/shineLogin.png"
+    },
+    {
+        id:3,
+        titulo:"Paleta de cores", 
+        descricao:"Paleta de cores para projetos figma",
+        imagem:"/paleta.png"
+    },
+    {
+        id:4,
+        titulo:"Real Aiko", 
+        descricao:"Desenho digital com referência fotográfica (Half body)",
+        imagem:"/nana.png"
+    },
+    {
+        id:5,
+        titulo:"Apolo", 
+        descricao:"Desenho digital com referência fotográfica (Half body)",
+        imagem:"/apolo.png"
+    },
+    {
+        id:6,
+        titulo:"Saiki", 
+        descricao:"Desenho digital Fanart (Icon)",
+        imagem:"/saiki.png"
+    },
+    {
+        id:7,
+        titulo:"Lily", 
+        descricao:"Desenho digital (Icon)",
+        imagem:"/lily.png"
+    },
+    {
+        id:8,
+        titulo:"Atlas", 
+        descricao:"Desenho digital com referência fotográfica (Half body)",
+        imagem:"/atlas.png"
+    },
+    {
+        id:9,
+        titulo:"Ícaro", 
+        descricao:"Desenho digital com referência fotográfica (Icon)",
+        imagem:"/icaro.png"
+    },
+    {
+        id:10,
+        titulo:"Aesthetic photo", 
+        descricao:"Desenho digital com referência fotográfica culinária (Icon)",
+        imagem:"/comida1.png"
+    },
+    {
+        id:11,
+        titulo:"Aesthetic photo", 
+        descricao:"Desenho digital com referência fotográfica culinária (Icon)",
+        imagem:"/comida2.png"
+    },
+    {
+        id:12,
+        titulo:"Hades", 
+        descricao:"Desenho digital com referência fotográfica (Icon)",
+        imagem:"/hades.png"
+    }
+)
 
-db.dono.insertMany([
-    {_id:1,nome:"Ana",idade:20,animal:[
-        {_id:1,nome:"Aik",valor:200.00,raca:"viralata"},
-        {_id:2,nome:"Lulu",valor:90.00,raca:"rapoza"},
-        {_id:3,nome:"Kika",valor:90.00,raca:"pug"}
-]},
-    {_id:2,nome:"Aiko",idade:25,animal:[
-        {_id:4,nome:"Tico",valor:200.00,raca:"viralata"},
-        {_id:5,nome:"Lilica",valor:90.00,raca:"rapoza"},
-        {_id:6,nome:"Koko",valor:90.00,raca:"pug"}
-]},
-    {_id:3,nome:"Andy",idade:35,animal:[
-        {_id:7,nome:"Saiko",valor:200.00,raca:"viralata"},
-        {_id:8,nome:"Pupu",valor:90.00,raca:"rapoza"},
-        {_id:9,nome:"Ju",valor:90.00,raca:"pug"}
-]},
-    {_id:4,nome:"Sergio",idade:21,animal:[
-        {_id:10,nome:"lily",valor:200.00,raca:"viralata"},
-        {_id:11,nome:"Pucca",valor:90.00,raca:"rapoza"},
-        {_id:12,nome:"Lala",valor:90.00,raca:"pug"}
-]}
-
-])
-
-
-//Questão 6
-db.dono.find({_id:2},{nome:true,animal:true});
-
-//Questão 7
-db.dono.deleteOne({_id:2})
-
-//Questão 8
-db.dono.find();
-
-//Questão 9
-db.dono.deleteMany({})
-
-//Questão 10
-db.dono.drop();
-
-//Questão 11
-db.dropDatabase();
+db.contratos.insertMany(
+    {
+        id:"",
+        nome:"",
+        sobrenome:"", 
+        mensagem:"",
+        contato:""
+    }
+)
